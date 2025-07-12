8fd42f86d9f9db664d0cbe80b068f9055d28b06e
Elharith Madani <harithmadani54@gmail.com>
2025-07-12 16:50:07 -0400

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import BackendConnect from "./components/BackendConnect";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NutriFit</h1>
+      <BackendConnect />
+    </div>
+  );
+}
+
+export default App;
